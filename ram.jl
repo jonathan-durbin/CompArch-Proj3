2508481a762d882fc453c14c24c016cb9d2f081b
jonathan-durbin <jongdurbin@gmail.com>
2019-12-13 23:40:34 -0500

    Translate from ram.scala.
    Mode 2 on quicksort.ram doesn't work,
    although it does work on bin-sort.ram

--- ram.jl
+++ ram.jl
@@ -1,20 +1,18 @@
 filename    = ARGS[1]
 mode        = ARGS[2]
 codeMode    = true
-cur         = 0
-mask        = 65535
+cur         = Int16(0)
 mem         = Dict{Int16, Int16}()
-mem_trunc   = Dict{Int, Int}()
+mem_trunc   = Dict{Int16, Int}()
 regs        = Dict{Char, Int}()
 new_regs    = Array{Char, 1}()
-new_mem     = Array{Array{Int, 1}, 1}()
-hazard      = tuple('0', '0', 0)  # (reg1, reg2, mem_location)
+new_mem     = Array{Array{Int16, 1}, 1}()
 
 
 "Write the value v to an address a in memory."
-function writeM(a:: Int, v:: Int16)
+function writeM(a:: Int16, v:: Int16)
     mem[a] = v
-    mode == "1" && push!(new_mem, Array{Int, 1}(a, mem[a]))
+    mode == "1" && push!(new_mem, Int16[a, mem[a]])
 end
 
 "Read from address a in memory, if address not present, return 0"
@@ -29,67 +27,89 @@ function writeR(a:: Char, v:: Int16)
 end
 
 "Output the value in register a. Registers 0-9 only hold their values."
-function readR(a:: Char):: Int
+function readR(a:: Char):: Int16
     '0' <= a <= '9' ?
         readValue(string(a)) :
-        get(regs, a, 0)
+        get(regs, a, Int16(0))
 end
 
-"Read the instruction at address a. Return "
-function readI(a:: Int16)
+"Return the instruction at address a. Only usable once instructions are loaded into memory."
+function readI(a:: Int16):: String
     x = readM(a)
-    y = readM(a+1)
+    y = readM(a+Int16(1))
     t = [
-        x & 0x00ff, x & 0xff00,
-        y & 0x00ff, y & 0xff00
+        x & 0xff, (x >> 8) & 0xff,
+        y & 0xff, (y >> 8) & 0xff
     ]
     String(map(x -> Char(x), t))
 end
 
-function readValue(a:: String)
+"Return the value `a` as an Int16. `a` can be hex (i.e., 0x1234) or dec (i.e., 1234)."
+function readValue(a:: String):: Int16
     startswith(a, "0x") ?
-        parse(Int, replace(a, "0x"=>""), base=16) :
-        parse(Int, a, base=10)
+        parse(Int16, replace(a, "0x"=>""), base=16) :
+        parse(Int16, a, base=10)
 end
+readValue(a:: SubString{String}) = readValue(string(a))
 
 
-stop = false
-writeSeen = false
+destReg = " "
+lastInst = ""
+println("Opening $filename, loading into memory.")
 open(filename, "r") do file
     global codeMode, cur
     for line in readlines(file)
-        stop && break
         if startswith(line, "code: ")
             address = split(replace(line, "code: "=>""))[1]
-            println("Address: $address")
             cur = readValue(String(address))
             codeMode = true
         elseif startswith(line, "data: ")
             header = split(replace(line, "data: "=>""))
             cur = readValue(String(header[1]))
             trunc = length(header) == 2 ? readValue(header[2]) : 0
-            mem_trunc(cur) = trunc
+            mem_trunc[cur] = Int(trunc)
             codeMode = false
         else
             if codeMode
                 code = rpad(strip(split(line, "#")[1]), 4)
                 if code != "    "
+                    # println(code)
                     writeM(
-                        cur,   Int16(Int(code[1]) % 256
-                            + (Int(code[2]) % 256) * 256)
+                        cur,   Int16(code[1]) % Int16(256)
+                            + (Int16(code[2]) % Int16(256)) * Int16(256)
                     )
                     writeM(
-                        cur+1, Int16(Int(code[3]) % 256
-                            + (Int(code[4]) % 256) * 256)
+                        cur+Int16(1), Int16(code[3]) % Int16(256)
+                            + (Int16(code[4]) % Int16(256)) * Int16(256)
                     )
                     if mode == "4"
-                        if !writeSeen
-                            nothing
-                        else
-                            nothing
+                        errString = "Hazard: $lastInst, $code"
+                        if code[1] in "BbEe<l>g"  # branches
+                            destReg in "$(code[2])" && println(errString)
+                            destReg = " "
+                            lastInst = code
+                        elseif code[1] in "+-*/%"  # maths
+                            destReg in "$(code[2])$(code[3])" && println(errString)
+                            destReg = string(code[4])
+                            lastInst = code
+                        elseif code[1] in "JI"  # jumps/immediates
+                            destReg = string(code[2])
+                            lastInst = code
+                        elseif '!' == code[1]  # syscalls
+                            destReg in "$(code[2])" && println(errString)
+                            destReg = string(code[2])
+                            lastInst = code
+                        elseif 'L' == code[1]  # loads
+                            destReg in "$(code[2])" && println(errString)
+                            destReg = string(code[3])
+                            lastInst = code
+                        elseif 'R' == code[1]  # returns
+                            destReg in "$(code[2])" && println(errString)
+                            destReg = " "
+                            lastInst = code
                         end
                     end
-                    cur += 2
+                    cur += Int16(2)
                 end
             else
                 data = map(
@@ -98,7 +118,7 @@ open(filename, "r") do file
                 )
                 for d in data
                     writeM(cur, d)
-                    cur += 1
+                    cur += Int16(1)
                 end
             end
         end
@@ -107,11 +127,12 @@ end
 
 
 # writeR('P', 0)
-inst = ""
 halt = false
-
+println("Executing memory.")
 while !halt
+    global new_regs, new_mem, halt
     inst = readI(readR('P'))
+    # println(inst)
     c = inst[1]
     inst = lstrip(inst, c)
 
@@ -121,7 +142,7 @@ while !halt
         a = readR(r)
         v = readM(a)
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     elseif c == 'S'
         s = inst[1]
@@ -129,7 +150,7 @@ while !halt
         a = readR(w)
         v = readR(s)
         writeM(a, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     # Mathematical operands
     elseif c == '+'
@@ -140,7 +161,7 @@ while !halt
         v2 = readR(s2)
         v  = v1 + v2
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     elseif c == '-'
         s1 = inst[1]
@@ -150,7 +171,7 @@ while !halt
         v2 = readR(s2)
         v = v1 - v2
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     elseif c == '*'
         s1 = inst[1]
@@ -160,7 +181,7 @@ while !halt
         v2 = readR(s2)
         v = v1 * v2
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     elseif c == '/'
         s1 = inst[1]
@@ -170,7 +191,7 @@ while !halt
         v2 = readR(s2)
         v = v1 / v2
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     elseif c == '%'
         s1 = inst[1]
@@ -180,64 +201,64 @@ while !halt
         v2 = readR(s2)
         v = v1 % v2
         writeR(d, v)
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
 
     # Branch operands
     elseif c == 'B'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) != 0 ?
-            writeR('P', readR('P') + readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') + readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == 'b'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) != 0 ?
-            writeR('P', readR('P') - readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') - readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == 'E'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) == 0 ?
-            writeR('P', readR('P') + readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') + readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == 'e'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) == 0 ?
-            writeR('P', readR('P') - readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') - readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == '<'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) < 0 ?
-            writeR('P', readR('P') + readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') + readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == 'l'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) < 0 ?
-            writeR('P', readR('P') - readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') - readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == '>'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) > 0 ?
-            writeR('P', readR('P') + readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') + readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     elseif c == 'g'
         s = inst[1]
         hh_hl = inst[2] * inst[3]
         readR(s) > 0 ?
-            writeR('P', readR('P') - readValue("0x" + hh_hl) * 2) :
-            writeR('P', readR('P') + 2)
+            writeR('P', readR('P') - readValue("0x" * hh_hl) * Int16(2)) :
+            writeR('P', readR('P') + Int16(2))
 
     # Return and halt
     elseif c == 'R'
@@ -248,14 +269,14 @@ while !halt
 
     # Jump and Imm
     elseif c == 'J'
-        imm = readValue("0x" * readI(readR('P') + 2))
-        writeR(inst[1], readR('P') + 4)
+        imm = readValue("0x" * readI(readR('P') + Int16(2)))
+        writeR(inst[1], readR('P') + Int16(4))
         writeR('P', imm)
 
     elseif c == 'I'
-        imm = readValue("0x" * readI(readR('P') + 2))
+        imm = readValue("0x" * readI(readR('P') + Int16(2)))
         writeR(inst[1], imm)
-        writeR('P', readR('P') + 4)
+        writeR('P', readR('P') + Int16(4))
 
     # Syscall
     elseif c == '!'
@@ -265,27 +286,41 @@ while !halt
         if hh_hl == "01"
             while readM(a) != 0
                 print(Char(readM(a)))
-                a += 1
+                a += Int16(1)
             end
         elseif hh_hl == "02"
             writeR(m, readValue(readline(stdin)))
         end
-        writeR('P', readR('P') + 2)
+        writeR('P', readR('P') + Int16(2))
     else
         @error("Illegal instruction!")
         break
     end
 
-    println(inst)
-
-    if mode == "1" println("$inst, R: $new_regs, M: $new_mem")
+    ms = !isempty(new_mem) ? ", M: " * string(new_mem) : ""
+    rs = !isempty(new_regs) ? ", R: " * string(new_regs) : ""
+    if mode == "1" println("$(c * inst)" * rs * ms)
     elseif !(mode in ["2", "3", "4"]) error("Not a valid mode!")
     end
 
-    new_regs = Array{Char}()
-    new_mem  = Array{Array{Int, Int}, 1}()
+    new_regs = Array{Char, 1}()
+    new_mem  = Array{Array{Int16, 1}, 1}()
 end  # execution block
 
 # println(regs)
+# println(mem_trunc)
 if mode == "2"
-    nothing
+    # output the values in each data section
+    for d in sort(collect(mem_trunc))
+        a = d[1]
+        print("Address: $(d[1])\nData:    ")
+        if d[2] != 0
+            for i = 1:d[2]
+                print(readM(a))
+                a += Int16(1)
+            end
+            print('\n')
+        end
+    end
+end
+
